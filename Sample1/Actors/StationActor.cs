517e3da9cc17fbc6cc4e6fd7c4b94941e70a0f3d
Jeremiah DeBoer <jay@Jeremiahs-MacBook-Pro.local>
2016-01-09 16:12:26 -0600

    Added current users list to station

--- Sample1/Actors/StationActor.cs
+++ Sample1/Actors/StationActor.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Akka.Actor;
 
 public class StationActor : ReceiveActor
@@ -9,21 +11,31 @@ public class StationActor : ReceiveActor
     }
     
     public int Id { get; private set; }
+    
+    public string[] CurrentUsers{
+        get { return _currentUsers.ToArray(); }
+    }
+    
     public StationActor(int id)
     {
         Id = id;
         Console.WriteLine("we were here");
         Receive<StationUserSendMessage>(m => OnReceivedStationUserSendMessage(m));
         Receive<StationUserLeftMessage>(m => OnRecievedStationUserLeftMessage(m));
+        _currentUsers = new HashSet<string>();
     }
+    
+    private readonly HashSet<string> _currentUsers;
     
     private void OnReceivedStationUserSendMessage(StationUserSendMessage message)
     {
+        _currentUsers.Add(message.UsersName);
         Console.WriteLine($"The user {message.UsersName} has arrived at station {Id}");
     }
     
     private void OnRecievedStationUserLeftMessage(StationUserLeftMessage message)
     {
+        _currentUsers.Remove(message.Name);
         Console.WriteLine($"The user {message.Name} has left station {Id}.");
     }
 }
